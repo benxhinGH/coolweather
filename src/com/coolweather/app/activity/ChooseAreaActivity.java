6eeaa68d8152799d643cbcf55588f9b83c659eeb
liuyouze <824797605@qq.com>
2016-03-14 22:34:17 +0800

    22:33第二阶段写到一半了，先提交一下，睡觉了

---
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -0,0 +1,53 @@
+package com.coolweather.app.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.CoolWeatherDB;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ChooseAreaActivity extends Activity{
+	public static final int LEVEL_PROVINCE=0;
+	public static final int LEVEL_CITY=1;
+	public static final int LEVEL_COUNTY=2;
+	
+	private ProgressDialog progressDialog;
+	private TextView titleText;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private CoolWeatherDB coolWeatherDB;
+	private List<String> dataList=new ArrayList<String>();
+	/**
+	 * 省列表
+	 */
+	private List<Province> provinceList;
+	/**
+	 * 市列表
+	 */
+	private List<City> cityList;
+	/**
+	 * 县列表
+	 */
+	private List<County> countyList;
+	/**
+	 * 选中的省份
+	 */
+	private Province selectedProvince;
+	/**
+	 * 选中的城市
+	 */
+	private City selectedCity;
+	/**
+	 * 当前选中的级别
+	 */
+	private int currentLevel;
+
+}
